70fb5631a9b67aec60257f76a613271f2f715c94
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2025-03-31 17:40:56 +0300

    edit found by id in server.js

--- src/server.js
+++ src/server.js
@@ -38,21 +38,32 @@ export const startServer = () => {
   });
 
   app.get('/exercises/:exerciseId', async (req, res, next) => {
-    const { exerciseId } = req.params;
-    const exercise = await getExerciseById(exerciseId);
+    try {
+      const { exerciseId } = req.params;
+      const exercise = await getExerciseById(exerciseId);
 
-    if (!exercise) {
-      res.status(404).json({
-        message: 'Exercise not found',
+      if (!exercise) {
+        res.status(404).json({
+          status: 404,
+          message: `Exercise with id=${exerciseId} not found`,
+        });
+        return;
+      }
+
+      res.status(200).json({
+        status: 200,
+        data: exercise,
+        message: `The exercise with id=${exerciseId} was found successfully.`,
       });
-      return;
+    } catch (error) {
+      if (error.message.includes('Cast to ObjectId failed')) {
+        error.status = 404;
+      }
+      const { status = 500 } = error;
+      res.status(status).json({
+        message: error.message,
+      });
     }
-
-    res.status(200).json({
-      status: 200,
-      data: exercise,
-      message: 'The exercise will be successfully completed.',
-    });
   });
 
   app.use('*', (req, res, next) => {
